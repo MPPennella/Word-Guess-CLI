9ee19b40e59fe0bdf58d07fd918bccc9d7a952e9
M. Pennella <michael.pennella@gmail.com>
2018-10-24 00:59:16 -0700

    Removed commented-out testing code

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// let Letter = require("./Letter.js")
-
-// let word = "SOMETHING"
-// let wordArr = [...word]
-
-// console.log(wordArr.map(helper).join(""))
-
-// function helper(ltr) {
-//     console.log(ltr)
-//     return new Letter(ltr)
-// }
-// let ltrArr=[];
-// for (let i=0; i<wordArr.length; i++) {
-//     let ltr = new Letter(wordArr[i])
-//     ltr.checkCharacter(wordArr[i])
-
-//     ltrArr.push(ltr)
-// }
-// console.log(ltrArr.join(""))
-
-// let Word = require("./Word.js")
-
-// let word = new Word("SOMETHING")
-// console.log(word.toString())
-// word.checkForLetter("O")
-// word.checkForLetter("W")
-// word.checkForLetter("T")
-// console.log(word.toString())
-
 const inquirer = require("inquirer");
 const Word = require("./Word.js");
 
