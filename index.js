4a8af0274b13ce8aef1e8d659376e49c37270a1d
M. Pennella <michael.pennella@gmail.com>
2018-10-24 01:06:34 -0700

    Added comments to guessLetter function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let wordToGuess = new Word(someWord.toUpperCase());
 guessLetter();
 
 function guessLetter() {
+    // Prompt for A-Z letter entry
     inquirer.prompt({
         message: "Guess a letter (A-Z):",
         name: "letter"
@@ -14,14 +15,21 @@ function guessLetter() {
         let letter = response.letter.trim().toUpperCase();
         console.log(letter);
 
+        // Check for valid entry
         if (letter.length==1 && letter>="A" && letter<="Z") {
             console.log("Valid letter")
+
+            // Check if letter is in word and update word
             wordToGuess.checkForLetter(letter);
             console.log(wordToGuess.toString());
 
+            // If word is not fully guessed, guess again
             guessLetter();
+            //Otherwise, player has won
         } else {
             console.log("Invalid letter")
+            // Ask if they want to try again
+
         }
     })
 }
